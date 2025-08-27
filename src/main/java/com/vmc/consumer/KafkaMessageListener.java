a6da96e3aaa9075c80777ac5d9cd7e4b928783b7
voorevamshi <vvamshijava@gmail.com>
2025-08-27 13:05:36 +0530

    VMC-25101 added initial project code for Kafka

---
+++ src/main/java/com/vmc/consumer/KafkaMessageListener.java
@@ -0,0 +1,32 @@
+package com.vmc.consumer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaMessageListener {
+
+    Logger log = LoggerFactory.getLogger(KafkaMessageListener.class);
+
+//    @KafkaListener(topics = "vmc-demo",groupId = "vmc-group")
+//    public void consumeEvents(Customer customer) {
+//        log.info("consumer consume the events {} ", customer.toString());
+//    }
+
+    @KafkaListener(topics = "vmc-demo",groupId = "vmc-group")
+    public void consume2(String message) {
+        log.info("consumer2 consume the message {} ", message);
+    }
+
+//    @KafkaListener(topics = "javatechie-demo1",groupId = "jt-group-new")
+//    public void consume3(String message) {
+//        log.info("consumer3 consume the message {} ", message);
+//    }
+//
+//    @KafkaListener(topics = "javatechie-demo1",groupId = "jt-group-new")
+//    public void consume4(String message) {
+//        log.info("consumer4 consume the message {} ", message);
+//    }
+}
